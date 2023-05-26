0b3a654762e5d1f21bb0017e47a51ef771b240e1
Rufai Sanni <sruffy07@gmail.com>
2023-05-26 16:20:57 +0100

    working with mock

--- NetworkUtility.Test/PingTests/NetworkServiceTest.cs
+++ NetworkUtility.Test/PingTests/NetworkServiceTest.cs
@@ -1,8 +1,12 @@
-﻿using FluentAssertions;
+﻿using FakeItEasy;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NetworkUtility.DNS;
 using NetworkUtility.Ping;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +14,24 @@ namespace NetworkUtility.Test.PingTests
 {
     public class NetworkServiceTest
     {
+        private readonly IDNS _dns;
+        private readonly NetworkService _pingService;
+        //SUT: System under testing
+        public NetworkServiceTest()
+        {
+            //Dependencies
+            _dns = A.Fake<IDNS>();
+
+            //SUT
+            _pingService = new NetworkService(_dns);
+        }
         [Fact]
-        public static void NetworkServiceTest_SendPing_ReturnString()
+        public void NetworkServiceTest_SendPing_ReturnString()
         {
             // Arrange
-            var pingService = new NetworkService();
-
+            A.CallTo(() => _dns.SendDNS()).Returns(true);
             // Act
-            var result = pingService.SendPing();
+            var result = _pingService.SendPing();
 
             //Assert
             result.Should().NotBeNullOrWhiteSpace();
@@ -29,18 +43,70 @@ namespace NetworkUtility.Test.PingTests
         [InlineData(1,1,2)]
         [InlineData(2, 2,4)]
 
-        public static void NetworkServiceTest_PingTimeOut_ReturnInt(int a, int b, int expected)
+        public void NetworkServiceTest_PingTimeOut_ReturnInt(int a, int b, int expected)
         {
             //Arrange
-            var pingTime = new NetworkService();
-
+            
             //Act
-            var result = pingTime.PingTimeOut(a, b);
+            var result = _pingService.PingTimeOut(a, b);
 
             //Assert
             result.Should().Be(expected);
             result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(-10000, 0);
+        }
+
+        [Fact]
+        public void NetworkServiceTest_LastPingDate_ReturnDate()
+        {
+            // Arrange
+            
+            // Act
+            var result = _pingService.LastPingDate();
+
+            //Assert 
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkServiceTest_GetPingOptions_ReturnObject()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.GetPingOptions();
+
+            //Assert 
+            //When testing for an object, we use "BeEquivalent"
+            //When testing for values, we use "Be"
+            //When testing for values, we use use an expected value
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        public void NetworkServiceTest_MostRecentPing_ReturnList()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.MostRecentPing();
+
+            //Assert 
+            result.Should().BeOfType<IEnumerable<PingOptions>>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
         }
     }
 }
--- NetworkUtility/Ping/NetworkService.cs
+++ NetworkUtility/Ping/NetworkService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NetworkUtility.DNS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +10,65 @@ namespace NetworkUtility.Ping
 {
     public class NetworkService
     {
+        private readonly IDNS _dNS;
+
+        public NetworkService(IDNS dNS)
+        {
+            _dNS = dNS;
+        }
         public string SendPing()
         {
-            return "Success: Ping Sent!";
+            var dnsSuccess = _dNS.SendDNS();
+            if(dnsSuccess)
+            {
+                return "Success: Ping Sent!";
+            }
+            else
+            {
+                return "Failed: Ping not sent!";
+            }
         }
 
         public int PingTimeOut(int a, int b )
         {
             return a + b;
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPing()
+        {
+            IEnumerable<PingOptions> pingOptions = new []
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                }
+            };
+            return pingOptions;
         }
     }
 }
